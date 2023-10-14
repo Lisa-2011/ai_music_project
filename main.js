e8f263b84896d6e0307205852e7b2e8675c88281
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2023-10-14 16:22:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 music1=""
 music2=""
 
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
     music1=loadSound("music.mp3");
     music2=loadSound("music2.mp3");
@@ -12,8 +17,27 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+
 }
 
 function draw(){
     image(video,0,0,600,500);
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        
+    }
 }
